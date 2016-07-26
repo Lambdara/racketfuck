5df1c99f62793c05becc093f857140bdcea1f941
Uwila <uwila@runbox.com>
2016-07-26 19:51:09 +0200

    Implement stubs, complete the program

--- racketfuck.rkt
+++ racketfuck.rkt
@@ -2,12 +2,9 @@
 
 (define memsize 30000) ;; Size of memory
 
-
-;(define args (current-command-line-arguments))
-
-(define args '#("helloworld.bf"))
-; Useful for testing 
-
+;; Vector containing input file as single element
+;; Change if not running from terminal
+(define args (current-command-line-arguments))
 
 ;; Check for arguments
 (if (not (= 1 (vector-length args)))
@@ -28,6 +25,7 @@
                       #\,
                       #\.))
 (define instructions (list->vector (filter (lambda (x) (member x symbols)) code)))
+(define ins-length (vector-length instructions))
 
 ;; Initialize memory
 (define memory (make-vector memsize))
@@ -37,19 +35,59 @@
   (+ x 1))
 (define (dec x)
   (- x 1))
+; Searches for matching `]`
+(define (seek-end insptr)
+  (define (go insptr counter)
+    (if (or (not (equal? (vector-ref instructions insptr) #\]))
+            (not (= 0 counter)))
+        (cond ((equal? (vector-ref instructions insptr) #\])
+               (go (inc insptr) (dec counter)))
+              ((equal? (vector-ref instructions (inc insptr)) #\[)
+               (go (inc insptr) (inc counter)))
+              (else
+               (go (inc insptr) counter)))
+        insptr))
+  (go insptr 0))
+; Searches for matching `[`
+(define (seek-start insptr)
+  (define (go insptr counter)
+    (if (or (not (equal? (vector-ref instructions insptr) #\[))
+            (not (= 0 counter)))
+        (cond ((equal? (vector-ref instructions insptr) #\[)
+               (go (dec insptr) (dec counter)))
+              ((equal? (vector-ref instructions (dec insptr)) #\])
+               (go (dec insptr) (inc counter)))
+              (else
+               (go (dec insptr) counter)))
+        insptr))
+  (go insptr 0))
 
+
+;; interpretation method
 ;; insptr is position in instructions
 ;; memptr is position in memory
 (define (run insptr memptr)
-  (match (vector-ref instructions insptr)
-    (#\+ (begin (vector-set! memory memptr (inc (vector-ref memory memptr)))
-                (run (inc insptr) memptr)))
-    (#\- (begin (vector-set! memory memptr (dec (vector-ref memory memptr)))
-                (run (inc insptr) (memptr))))
-    (#\> (run (inc insptr) (inc memptr)))
-    (#\< (run (inc insptr) (dec memptr)))
-    (#\[ (run (seek-end insptr) memptr))
-    (#\] (run (seek-start insptr) memptr))
-    (#\, (begin (vector-set! memory memptr (char->integer (read-char)))
-                (run (inc insptr) memptr)))
-    (#\. (begin (display (integer->char (vector-ref memory memptr)))))))
+  (if (< insptr ins-length)
+      (match (vector-ref instructions insptr)
+        (#\+ (begin (vector-set! memory memptr (inc (vector-ref memory memptr)))
+                    (run (inc insptr) memptr)))
+        (#\- (begin (vector-set! memory memptr (dec (vector-ref memory memptr)))
+                    (run (inc insptr) memptr)))
+        (#\> (run (inc insptr) (inc memptr)))
+        (#\< (run (inc insptr) (dec memptr)))
+        (#\[ (run (if (= 0 (vector-ref memory memptr))
+                      (inc (seek-end insptr))
+                      (inc insptr))
+                  memptr))
+        (#\] (run (if (= 0 (vector-ref memory memptr))
+                      (inc insptr)
+                      (inc (seek-start insptr)))
+                  memptr))
+        (#\, (begin (vector-set! memory memptr (char->integer (read-char)))
+                    (run (inc insptr) memptr)))
+        (#\. (begin (display (integer->char (vector-ref memory memptr)))
+                    (run (inc insptr) memptr))))
+      (newline)))
+
+;; Start interpretation
+(run 0 0)
